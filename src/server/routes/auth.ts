5439956dd746bd2b2f54bf255c1946ddcc0c347e
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-17 18:47:25 +0000

    update: login endpoint

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -1,22 +1,81 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from "fastify-plugin";
+import { responseErrors } from "../../data/schema/responseErrors";
+import {
+  userLoginResponseSchema,
+  userLoginSchema,
+} from "../../data/schema/user.schema";
+import { RoutePrefix } from "../types";
 
 async function authRoutes(
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
 ) {
-  const prefixedPath = options.prefix || "/auth";
+  const prefixedPath = options.prefix || RoutePrefix.AUTH;
 
-  fastify.post(prefixedPath + "/login", async (request, reply) => {
-    const { email, password } = request.body as {
+  fastify.post<{
+    Body: {
       email: string;
       password: string;
     };
+    Reply: { message: string; data?: { token: string }; errors?: any };
+  }>(
+    prefixedPath + RoutePrefix.LOGIN,
+    {
+      schema: {
+        body: userLoginSchema,
+        response: {
+          200: userLoginResponseSchema,
+          ...responseErrors,
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body;
 
-    if (!email || !password) {
-      return reply.status(400).send({ message: "Bad credentials." });
-    }
-  });
+      if (!email || !password) {
+        return reply.status(400).send({ message: "Bad credentials." });
+      }
+
+      try {
+        fastify.log.debug(`Attempting to authenticate: ${email}/${password}`);
+
+        const userRepository = fastify.db.userRepository;
+        if (!userRepository) {
+          throw new Error("User repository is not initialized.");
+        }
+
+        const user = await userRepository.findOne({
+          where: { email },
+        });
+        if (!user) {
+          return reply.status(404).send({ message: "User not found." });
+        }
+
+        if (!user.isActive) {
+          return reply.status(403).send({ message: "User is not active." });
+        }
+
+        if (!(await user.checkPassword(password))) {
+          return reply.status(401).send({ message: "Bad credentials." });
+        }
+
+        const token = fastify.jwt.sign({ id: user.id, email: user.email });
+
+        if (!token) {
+          throw new Error("No token.");
+        }
+
+        reply.status(200).send({
+          message: "Login successful.",
+          data: { token },
+        });
+      } catch (error) {
+        fastify.log.error(`Authentication error: ${error.message}`);
+        return reply.status(500).send({ message: "Internal server error." });
+      }
+    },
+  );
 }
 
 export default fp(authRoutes, {
