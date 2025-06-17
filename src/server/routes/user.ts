5ea7a7bd600c2938f806e76d9fd34c0f62a6675c
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-17 18:48:14 +0000

    update: user endpoint

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -2,38 +2,107 @@ import { validate } from "class-validator";
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from "fastify-plugin";
 
-import { AppDataSource } from "../../data/data-source";
 import { Person, PersonUpdateType } from "../../data/entity/person.entity";
 import { User } from "../../data/entity/user.entity";
+import { responseErrors } from "../../data/schema/responseErrors";
 import {
   createUserBodySchema,
   userResponseSchema,
+  userResponseSchemaIncludePerson,
 } from "../../data/schema/user.schema";
+import { Role } from "../../data/types";
 import { hashPassword } from "../../data/utils";
+import { RoutePrefix } from "../types";
 
 async function userRoutes(
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
 ) {
-  const prefixedPath = options.prefix || "/account";
+  const prefixedPath = options.prefix || RoutePrefix.USER;
 
-  fastify.get(prefixedPath, async (request, reply) => {
-    const users = await AppDataSource.manager.find(User);
-    return { message: "List of users", data: users };
-  });
-
-  fastify.get(prefixedPath + "/:id", async (request, reply) => {
-    const userId = (request.params as { id: string }).id;
-    const user = await AppDataSource.manager.findOne(User, {
-      where: { id: parseInt(userId) },
-    });
-    return {
-      message: user
-        ? `Details for account ${userId}`
-        : `Account ${userId} not found`,
-      data: user,
+  fastify.get<{
+    Reply: {
+      message: string;
+      data?: Array<User>;
     };
-  });
+  }>(
+    prefixedPath,
+    {
+      schema: {
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              message: { type: "string" },
+              data: { type: "array", items: userResponseSchema },
+            },
+            required: ["message", "data"],
+          },
+          ...responseErrors,
+        },
+      },
+      onRequest: [fastify.authenticate({ role: Role.ADMIN })],
+    },
+    async (request, reply) => {
+      try {
+        const userRepository = fastify.db.userRepository;
+        const users = await userRepository.find();
+        return reply
+          .status(200)
+          .send({ message: "List of users", data: users });
+      } catch (error) {
+        fastify.log.error(`Error fetching users: ${error}`);
+        return reply.status(500).send({ message: "Internal server error." });
+      }
+    },
+  );
+
+  fastify.get<{
+    Reply: {
+      message: string;
+      data?: User;
+    };
+  }>(
+    prefixedPath + "/:id",
+    {
+      schema: {
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              message: { type: "string" },
+              data: userResponseSchema,
+            },
+            required: ["message", "data"],
+          },
+          ...responseErrors,
+        },
+      },
+      onRequest: [fastify.authenticate({ allowSelf: true })],
+    },
+    async (request, reply) => {
+      const userId = (request.params as { id: string }).id;
+      try {
+        const userRepository = fastify.db.userRepository;
+        const user = await userRepository.findOne({
+          where: { id: parseInt(userId) },
+        });
+
+        if (!user) {
+          return reply
+            .status(404)
+            .send({ message: `User id:${userId} not found.` });
+        }
+
+        return reply
+          .status(200)
+          .send({ message: `Details for account id:${userId}`, data: user });
+      } catch (error) {
+        fastify.log.error(`Error fetching user: ${error}`);
+        return reply.status(500).send({ message: "Internal server error." });
+      }
+    },
+  );
 
   fastify.post<{
     Body: {
@@ -52,30 +121,14 @@ async function userRoutes(
       schema: {
         body: createUserBodySchema,
         response: {
-          201: userResponseSchema,
-          400: {
-            type: "object",
-            properties: {
-              message: { type: "string" },
-              errors: { type: "array", items: { type: "string" } },
-            },
-          },
-          404: { type: "object", properties: { message: { type: "string" } } },
-          409: { type: "object", properties: { message: { type: "string" } } },
-          500: { type: "object", properties: { message: { type: "string" } } },
+          201: userResponseSchemaIncludePerson,
+          ...responseErrors,
         },
       },
       // Pre-handler hook to resolve/create the Person entity
       preHandler: async (request, reply) => {
         const { person: personData } = request.body;
-        // const personRepository = request.db.personRepository;
-        const personRepository = request.server.db.personRepository;
-        if (!request.server.db) {
-          fastify.log.error("request.server.db is undefined!");
-          return reply
-            .status(500)
-            .send({ message: "Internal Server Error: DB not loaded" });
-        }
+        const personRepository = fastify.db.personRepository;
 
         let resolvedPerson: Person | null = null;
 
@@ -120,7 +173,7 @@ async function userRoutes(
           try {
             resolvedPerson = await personRepository.save(newPerson);
           } catch (error) {
-            fastify.log.error("Error creating new person:", error);
+            fastify.log.error(`Error creating new person: ${error}`);
             return reply
               .status(500)
               .send({ message: "Failed to create new person." });
@@ -132,9 +185,9 @@ async function userRoutes(
     async (request, reply) => {
       const { email, password, isActive, role, language, timezone } =
         request.body;
-      const accountRepository = request.server.db.accountRepository;
-      if (!accountRepository) {
-        fastify.log.error("accountRepository is undefined!");
+      const userRepository = fastify.db.userRepository;
+      if (!userRepository) {
+        fastify.log.error("userRepository is undefined!");
         return reply
           .status(500)
           .send({ message: "Internal Server Error: DB not loaded" });
@@ -163,7 +216,7 @@ async function userRoutes(
       }
 
       try {
-        const savedUser = await accountRepository.save(newUser);
+        const savedUser = await userRepository.save(newUser);
         // To ensure the 'person' relation is loaded in the response,
         // we might need to explicitly load it or return a fresh find.
         // For simplicity, we'll return the savedAccount, but be aware TypeORM
