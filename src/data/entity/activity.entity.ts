5e2a60cf9661438759626b8815eadc94a0d337b6
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-23 08:06:28 +0000

    add: entities

---
+++ src/data/entity/activity.entity.ts
@@ -0,0 +1,34 @@
+import { IsNotEmpty, IsString, Length } from "class-validator";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import Category from "./category.entity";
+
+@Entity()
+export default class Activity {
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  @Length(100)
+  title: string;
+
+  @ManyToOne(() => Category, (category) => category.activities, {
+    nullable: true,
+  })
+  @JoinColumn({ name: "categoryId" })
+  category: Category;
+
+  @Column({ nullable: true })
+  categoryId: number;
+}
