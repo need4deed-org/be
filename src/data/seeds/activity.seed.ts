24aecec108acf285cc30131a3bf84111ca428ac4
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-24 14:11:04 +0000

    add: entities

---
+++ src/data/seeds/activity.seed.ts
@@ -0,0 +1,62 @@
+import { DataSource } from "typeorm";
+
+import { seedActivityFile } from "../../config/constants";
+import Activity from "../entity/activity.entity";
+import Category from "../entity/category.entity";
+import { readJsonAsync } from "../utils";
+
+interface ActivityJSON {
+  id: string;
+  category: string;
+}
+
+export async function seedActivity(dataSource: DataSource): Promise<void> {
+  if (!dataSource) {
+    throw new Error("DataSource is not initialized.");
+  }
+
+  const activityRepository = dataSource.getRepository(Activity);
+  if (!activityRepository) {
+    throw new Error("Activity entity is not initialized.");
+  }
+
+  const categoryRepository = dataSource.getRepository(Category);
+  if (!categoryRepository) {
+    throw new Error("Category entity is not initialized.");
+  }
+
+  const categories = await categoryRepository.find();
+
+  const activities = (await readJsonAsync(seedActivityFile)) as ActivityJSON[];
+
+  const existingActivities = new Set(
+    (await activityRepository.find()).map((activity) => activity.title),
+  );
+
+  const activitiesForInsert = activities.reduce(
+    (result: Activity[], { id, category }) => {
+      if (existingActivities.has(id)) {
+        return result;
+      }
+      try {
+        const newActivity = new Activity(id);
+        const categoryObject = categories.find((cat) => cat.title === category);
+        if (!categoryObject) {
+          throw new Error(`Error: category ${category} not found.`);
+        }
+        newActivity.categoryId = categoryObject.id;
+        result.push(newActivity);
+      } catch (error) {
+        throw new Error(`Error creating new activity ${id}: ${error.message}`);
+      }
+      return result;
+    },
+    [],
+  );
+
+  try {
+    await activityRepository.insert(activitiesForInsert);
+  } catch (error) {
+    throw new Error(`Error inserting activities: ${error.message}`);
+  }
+}
