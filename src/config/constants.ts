501dfbf8019ea7d70c45715f4a516b8be389eb2b
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-24 13:22:06 +0000

    add: data

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -13,3 +13,21 @@ export const seedLanguageInUseFile = path.join(
   ...publicFixturesFromHere,
   "languages_in_use.json",
 );
+
+export const seedCategoryFile = path.join(
+  __dirname,
+  ...publicFixturesFromHere,
+  "categories.json",
+);
+
+export const seedActivityFile = path.join(
+  __dirname,
+  ...publicFixturesFromHere,
+  "activities.json",
+);
+
+export const seedSkillFile = path.join(
+  __dirname,
+  ...publicFixturesFromHere,
+  "skills.json",
+);
