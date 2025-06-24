e76ea77c121d4b2f9e7f736bd40cbfe83f30d3d0
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-24 14:13:50 +0000

    add: seedings

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,16 @@
 import { AppDataSource } from "./data-source";
+import { seedActivity } from "./seeds/activity.seed";
+import { seedCategory } from "./seeds/category.seed";
 import { seedFieldTranslation } from "./seeds/field_translation.seed";
 import { seedLanguage } from "./seeds/language.seed";
+import { seedSkill } from "./seeds/skill.seed";
 
 AppDataSource.initialize()
   .then(() => {
     seedLanguage(AppDataSource);
+    seedCategory(AppDataSource);
+    seedActivity(AppDataSource);
+    seedSkill(AppDataSource);
     seedFieldTranslation(AppDataSource);
   })
   .catch((error) => {
