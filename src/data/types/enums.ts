b5a226c6c56d08fe20f370562c2fed1fbbe32c86
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-21 16:41:00 +0000

    add: countries and german cities

--- src/data/types/enums.ts
+++ src/data/types/enums.ts
@@ -1,4 +1,14 @@
+import { Lang } from "need4deed-sdk";
+
 export enum Role {
   USER = "user",
   ADMIN = "admin",
+}
+
+export const Country = Lang;
+
+// this will go to sdk
+export enum GermanCity {
+  BERLIN = "Berlin",
+  POTSDAM = "Potsdam",
 }
