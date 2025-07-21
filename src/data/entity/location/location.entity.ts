5a22e957138abc292140ef9f6b2391396f67a786
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-21 17:40:55 +0000

    add: location entity

---
+++ src/data/entity/location/location.entity.ts
@@ -0,0 +1,35 @@
+import { IsOptional, IsString } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { LocationType } from "../../types";
+import LocationPostcode from "../m2m/location-postcode";
+
+@Entity()
+export default class Location {
+  constructor(location?: Partial<Location>) {
+    if (location) {
+      Object.assign(this, location);
+    }
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "enum",
+    enum: LocationType,
+    nullable: false,
+    default: LocationType.DISTRICT,
+  })
+  type: LocationType;
+
+  @Column({ nullable: true })
+  @IsOptional()
+  @IsString()
+  info: string;
+
+  @OneToMany(
+    () => LocationPostcode,
+    (locationPostcode) => locationPostcode.location,
+  )
+  locationPostcode: LocationPostcode[];
+}
