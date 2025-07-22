3596e1c3f116da19a2db630e9fef663d951b24fe
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-22 07:33:50 +0000

    add: deal entity

---
+++ src/data/entity/deal.entity.ts
@@ -0,0 +1,57 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { DealType } from "../types";
+import Location from "./location/location.entity";
+import Profile from "./profile/profile.entity";
+import Time from "./time/time.entity";
+
+@Entity()
+export default class Deal {
+  constructor(deal?: Partial<Deal>) {
+    if (deal) {
+      Object.assign(this, deal);
+    }
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "enum",
+    enum: DealType,
+    nullable: false,
+  })
+  type: DealType;
+
+  @ManyToOne(() => Profile, (profile) => profile.deal, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "profile_id" })
+  profile: Profile;
+
+  @Column()
+  postcodeId: number;
+
+  @ManyToOne(() => Time, (time) => time.deal, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "time_id" })
+  time: Time;
+
+  @Column()
+  timeId: number;
+
+  @ManyToOne(() => Location, (location) => location.deal, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "location_id" })
+  location: Location;
+
+  @Column()
+  locationId: number;
+}
