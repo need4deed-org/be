04bedceff3f0f6daadba0e6158e4319b4b5dc2c7
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-30 15:10:48 +0000

    add: oppportunity entity

---
+++ src/data/entity/opportunity/opportunity.entity.ts
@@ -0,0 +1,50 @@
+import { IsOptional, IsString } from "class-validator";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { OpportunityType } from "../../types";
+import Deal from "../deal.entity";
+import Agent from "./agent.entity";
+
+@Entity()
+export default class Opportunity {
+  constructor(opportunity?: Partial<Opportunity>) {
+    if (opportunity) {
+      Object.assign(this, opportunity);
+    }
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "enum",
+    enum: OpportunityType,
+    nullable: false,
+    default: OpportunityType,
+  })
+  type: OpportunityType;
+
+  @Column()
+  @IsOptional()
+  @IsString()
+  info: string;
+
+  @ManyToOne(() => Deal)
+  @JoinColumn({ name: "deal_id" })
+  deal: Deal;
+
+  @Column({ nullable: true })
+  dealId: number;
+
+  @ManyToOne(() => Agent)
+  @JoinColumn({ name: "agent_id" })
+  agent: Agent;
+
+  @Column({ nullable: true })
+  agentId: number;
+}
