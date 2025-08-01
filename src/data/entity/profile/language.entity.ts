5ecb0a8ce9cb63ab104af9ba92044ea54a878069
arturasmckwcz <arturasmckwcz@gmail.com>
2025-08-01 20:09:26 +0000

    add: reverse relation to event

--- src/data/entity/profile/language.entity.ts
+++ src/data/entity/profile/language.entity.ts
@@ -1,5 +1,7 @@
 import { IsNotEmpty, IsString, Length } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import EventN4D from "../event/event.entity";
+import EventTranslation from "../event/event_translation.entity";
 import ProfileLanguage from "../m2m/profile-language";
 
 @Entity()
@@ -29,4 +31,13 @@ export default class Language {
     (profileLanguage) => profileLanguage.language,
   )
   profileLanguage: ProfileLanguage[];
+
+  @OneToMany(() => EventN4D, (eventn4d) => eventn4d.language)
+  eventn4d: EventN4D[];
+
+  @OneToMany(
+    () => EventTranslation,
+    (event_translation) => event_translation.language,
+  )
+  event_translation: EventTranslation[];
 }
