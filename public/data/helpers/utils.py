947d4e766f7ce091eec0aa71399434d70ea0397c
arturasmckwcz <arturasmckwcz@gmail.com>
2025-08-09 19:45:50 +0000

    update: refactor by moving some code to utils

--- public/data/helpers/utils.py
+++ public/data/helpers/utils.py
@@ -1,5 +1,70 @@
+def get_name_fields(name):
+    """
+    Extracts the names from the string.
+    """
+    name_fields = {
+        "first_name": None,
+        "last_name": None,
+        "middle_name": None,
+    }
+    if not isinstance(name, str) or len(name.strip()) == 0:
+        return name_fields
+    
+    names = [item.strip() for item in name.split(" ") if item.strip()]
+    
+    name_fields["first_name"] = names[0]
+    if len(names) == 2:
+        name_fields["last_name"] = names[1]
+        return name_fields
+    
+    if len(names) > 2:
+        name_fields["last_name"] = names[-1]
+        name_fields["middle_name"] = " ".join(names[1:-1])
+
+    return name_fields
+
+def get_email(email):
+    """
+    Extracts the email from the string.
+    """
+    if not isinstance(email, str):
+        return None
+    
+    email = email.strip().lower()
+    if "@" in email and "." in email:
+        if email.startswith("mailto:"):
+            email = email[7:]
+        return email
+    
+    return None
 
 def get_list(lst):
-    if len(lst) == 1 and not lst[0]:
-        return []
-    return lst
+    return [item for item in lst if item]
+
+def get_string_or_null(value):
+    """
+    Returns the value if it exists, otherwise returns None.
+    """
+
+    return value if len(str(value)) else None
+
+def get_timeslot_data(timeslot):
+    """
+    Extracts timeslot data from a string.
+    4 variants of slots:
+    - "Monday 8.00 - 10.00 10.00 - 12.00 12.00 - 14.00 14.00 - 16.00 16.00 - 18.00 18.00 - 20.00"
+    - "Friday 14-17 | 17-20 | 11-14 | 08-11"
+    - "Tuesday afternoon | noon | morning"
+    - "Friday Not available"
+    "Days" might be ""
+    """
+
+    timeslot = timeslot.replace(" - ", "-")
+    day, *slots = timeslot.split(" ")
+    if not day or " ".join(slots) == "Not available":
+        return None
+    
+    if not slots:
+        return {"day": day, "daytime": []}
+    
+    return {"day": day, "daytime": [slot.strip() for slot in slots if slot.strip() and slot.strip() != "|"]}
