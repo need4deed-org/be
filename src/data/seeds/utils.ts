73c9415700a51c48706cb6ac59e7a8c6ea6fc9f8
arturasmckwcz <arturasmckwcz@gmail.com>
2025-08-17 22:26:04 +0000

    update: languages map and add districts for later

--- src/data/seeds/utils.ts
+++ src/data/seeds/utils.ts
@@ -13,6 +13,8 @@ export async function getLanguage(
       Bengal: "Bengali",
       Greek: "Modern Greek (1453-)",
       Kurmanji: "Northern Kurdish",
+      Kurmanci: "Northern Kurdish",
+      Northern: "Northern Kurdish",
       Sorani: "Central Kurdish",
       "Farsi/Dari": "Persian",
       Punjabi: "Panjabi",
@@ -76,3 +78,85 @@ export function getDocumentStatus(status: string): DocumentStatusType {
   };
   return statusMap[status] || DocumentStatusType.UNDEFINED;
 }
+
+export function getStartEnd(startEnd: string): { start: Date; end: Date } {
+  const map = {
+    not: null,
+    Nicht: null,
+    verfügbar: null,
+    available: null,
+    "8:00 - 10:00": { startHour: 8, endHour: 10 },
+    "10:00 - 12:00": { start: 10, end: 12 },
+    "14:00 - 16:00": { startHour: 14, endHour: 16 },
+    "16:00 - 18:00": { startHour: 16, endHour: 18 },
+    "18:00 - 20:00": { startHour: 18, endHour: 20 },
+    "14-17": { startHour: 14, endHour: 17 },
+    "17-20": { startHour: 17, endHour: 20 },
+    "11-14": { startHour: 11, endHour: 14 },
+    "08-11": { startHour: 8, endHour: 11 },
+    morning: { startHour: 8, endHour: 10 },
+    noon: { start: 11, end: 13 },
+    afternoon: { start: 14, end: 16 },
+    evening: { start: 17, end: 20 },
+  };
+
+  if (!map[startEnd]) {
+    return null;
+  }
+
+  const { startHour, endHour } = map[startEnd];
+
+  const start = new Date("2024-01-01");
+  start.setHours(startHour, 0, 0, 0);
+  const end = new Date("2024-01-01");
+  end.setHours(endHour, 0, 0, 0);
+
+  return { start, end };
+}
+
+const districts = {
+  Mitte: "",
+  "Friedrichshain-Kreuzberg": "",
+  Pankow: "",
+  "Charlottenburg-Wilmersdorf": "",
+  Spandau: "",
+  "Steglitz-Zehlendorf": "",
+  "Tempelhof-Schöneberg": "",
+  Neukölln: "",
+  "Treptow-Köpenick": "",
+  "Marzahn-Hellersdorf": "",
+  Lichtenberg: "",
+  Reinickendorf: "",
+};
+
+const mapDistricts = {
+  Charlottenburg: "",
+  Freidrichshain: "",
+  Friedrichshain: "",
+  Hellersdorf: "",
+  Kreuzberg: "",
+  Köpenick: "",
+  Lichtenberg: "",
+  Marzahn: "",
+  "Marzahn-Hellersdorf": "",
+  Mitte: "",
+  Moabit: "",
+  Neukölln: "",
+  Pankow: "",
+  "Phone translation": "",
+  "Prenzlauer Berg": "",
+  Reinickendorf: "",
+  Remotely: "",
+  Rudow: "",
+  Schöneberg: "",
+  Spandau: "",
+  Steglitz: "",
+  Tegel: "",
+  Telefonisch: "",
+  Tempelhof: "",
+  Treptow: "",
+  Wedding: "",
+  Weißensee: "",
+  Wilmersdorf: "",
+  Zehlendorf: "",
+};
