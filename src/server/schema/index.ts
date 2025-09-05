c865efdbbf553724b4ff980a3de9f1e006a1eda1
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-05 12:33:31 +0000

    update: schema and serializer

--- src/server/schema/index.ts
+++ src/server/schema/index.ts
@@ -2,5 +2,6 @@ export * from "./person.schema";
 export * from "./responseErrors";
 export * from "./user.schema";
 
-import volunteerApi from "./volunteer-api.json";
-export const volunteerResponseSchema = volunteerApi.definitions.VolunteerAPI;
+import volunteerSchemaJson from "./volunteer-api.json";
+export const volunteerResponseSchema =
+  volunteerSchemaJson.definitions.ApiVolunteerGetList;
