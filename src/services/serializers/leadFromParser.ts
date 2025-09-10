83a17c7a553f53c70f94af93c37a0d196652d22a
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-10 15:21:07 +0000

    add: parser

--- src/services/serializers/leadFromParser.ts
+++ src/services/serializers/leadFromParser.ts
@@ -1,5 +1,26 @@
 import { fastify } from "../../server";
 
-export async function leadFromParser(leadFromData: string[]): Promise<void> {
+import LeadFrom from "../../data/entity/lead.entity";
+import { TranslationEntityType } from "../../data/types";
+import { getInstanceByTranslation } from "../../server/utils";
+
+export async function leadFromParser(
+  leadFromData: string[],
+): Promise<LeadFrom[]> {
   fastify.log.debug(`Lead from data: ${JSON.stringify(leadFromData)}`);
+
+  const leadFrom: LeadFrom[] = [];
+  const lead = (leadFromData || []) as string[];
+  for (const volunteerLead of lead) {
+    const leadInstance = await getInstanceByTranslation(
+      volunteerLead,
+      LeadFrom,
+      TranslationEntityType.LEAD,
+    );
+    if (leadInstance) {
+      leadFrom.push(leadInstance);
+    }
+  }
+
+  return leadFrom;
 }
