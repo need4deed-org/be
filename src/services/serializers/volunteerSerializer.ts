8149a6d53cf28d5feca8d759d891731b0d4bbd28
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-11 12:32:53 +0000

    fix: wrap serializer into try-catch

--- src/services/serializers/volunteerSerializer.ts
+++ src/services/serializers/volunteerSerializer.ts
@@ -4,24 +4,48 @@ import ProfileLanguage from "../../data/entity/m2m/profile-language";
 import TimeTimeslot from "../../data/entity/m2m/time-timeslot";
 import Person from "../../data/entity/person.entity";
 import Volunteer from "../../data/entity/volunteer/volunteer.entity";
+import { fastify } from "../../server";
 
 export function volunteerSerializer(volunteer: Volunteer): ApiVolunteerGetList {
-  return {
-    name: getName(volunteer.person),
-    languages: getLanguages(volunteer.deal.profile.profileLanguage),
-    availability: getAvailability(volunteer.deal.time.timeTimeslot),
-    activities: getTitles(volunteer.deal.profile.profileActivity, "activity"),
-    skills: getTitles(volunteer.deal.profile.profileSkill, "skill"),
-    locations: getTitles(volunteer.deal.location.locationDistrict, "district"),
-  };
+  try {
+    const id = volunteer.id;
+    const name = getName(volunteer.person);
+    const languages = getLanguages(volunteer.deal.profile.profileLanguage);
+    const availability = getAvailability(volunteer.deal.time.timeTimeslot);
+    const activities = getTitles(
+      volunteer.deal.profile.profileActivity,
+      "activity",
+    );
+    const skills = getTitles(volunteer.deal.profile.profileSkill, "skill");
+    const locations = getTitles(
+      volunteer.deal.location.locationDistrict,
+      "district",
+    );
+    return {
+      id,
+      name,
+      languages,
+      availability,
+      activities,
+      skills,
+      locations,
+    };
+  } catch (error) {
+    fastify.log.error(
+      `Error serializing volunteer (id:${volunteer.id}): ${error}`,
+    );
+  }
 }
 
 function getByDay(rrule: string): ByDay {
-  const byDayPos = rrule.indexOf("BYDAY") + 6;
+  if (!rrule) {
+    throw new Error("RRule is required to get ByDay");
+  }
+  const byDayPos = rrule?.indexOf("BYDAY") + 6;
   if (byDayPos < 6) {
     return null;
   }
-  const byDay = rrule.slice(byDayPos, byDayPos + 2);
+  const byDay = rrule?.slice(byDayPos, byDayPos + 2);
   if (!(byDay in ByDay)) {
     return null;
   }
@@ -29,15 +53,20 @@ function getByDay(rrule: string): ByDay {
 }
 
 function getHour(date: Date): Hour {
+  if (!date) {
+    throw new Error("Date is required to get hour");
+  }
   const hour = `H${String(date.getHours()).padStart(2, "0")}`;
   return Hour[hour];
 }
 
 function getAvailability(timeTimeslot: TimeTimeslot[]) {
-  return timeTimeslot.map(({ timeslot }) => ({
-    day: getByDay(timeslot.rrule),
-    daytime: [getHour(timeslot.start), getHour(timeslot.end)] as Daytime,
-  }));
+  return timeTimeslot.map(({ timeslot }) => {
+    return {
+      day: getByDay(timeslot.rrule),
+      daytime: [getHour(timeslot.start), getHour(timeslot.end)] as Daytime,
+    };
+  });
 }
 
 function getName(person: Person) {
