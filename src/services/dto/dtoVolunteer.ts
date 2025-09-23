2aaba03aa21c068737c1ea8201b391dcae72fab6
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-23 11:48:57 +0000

    add: single volunteer DTO

---
+++ src/services/dto/dtoVolunteer.ts
@@ -0,0 +1,155 @@
+import {
+  Address,
+  ApiVolunteerGet,
+  ApiVolunteerGetList,
+  Availability,
+  ByDay,
+  Daytime,
+  Hour,
+  Occasionally,
+  Option,
+  TimedText,
+} from "need4deed-sdk";
+
+import ProfileLanguage from "../../data/entity/m2m/profile-language";
+import TimeTimeslot from "../../data/entity/m2m/time-timeslot";
+import Volunteer from "../../data/entity/volunteer/volunteer.entity";
+import { fastify } from "../../server";
+
+const city = "Berlin";
+
+export function volunteerListSerializer(
+  volunteer: Volunteer,
+): ApiVolunteerGetList {
+  try {
+    const id = volunteer.id;
+    const status = volunteer.status;
+    const name = volunteer.person.name;
+    const avatarUrl = volunteer.person?.avatarUrl || null;
+    const languages = getLanguages(volunteer.deal.profile.profileLanguage);
+    const availability = getAvailability(volunteer.deal.time.timeTimeslot);
+    const activities = getTitles(
+      volunteer.deal.profile.profileActivity,
+      "activity",
+    );
+    const skills = getTitles(volunteer.deal.profile.profileSkill, "skill");
+    const locations = getTitles(
+      volunteer.deal.location.locationDistrict,
+      "district",
+    );
+    return {
+      id,
+      status,
+      name,
+      avatarUrl,
+      languages,
+      availability,
+      activities,
+      skills,
+      locations,
+    };
+  } catch (error) {
+    fastify.log.error(
+      `Error serializing volunteer (id:${volunteer.id}): ${error}`,
+    );
+  }
+}
+
+export function volunteerSerializer(volunteer: Volunteer): ApiVolunteerGet {
+  const address: Address = {
+    ...volunteer.person.address,
+    id: String(volunteer.person.address.id),
+    city,
+    postcode: {
+      ...volunteer.person.address.postcode,
+      id: String(volunteer.person.address.postcode.id),
+      code: volunteer.person.address.postcode.value,
+      latitude: volunteer.person.address.postcode.latitude || null,
+      longitude: volunteer.person.address.postcode.longitude || null,
+    },
+  };
+
+  const comments = [] as unknown as TimedText[];
+  const email = volunteer.person.email;
+  const firstName = volunteer.person.firstName;
+  const goodConductCertificate = volunteer.statusCGC;
+  const infoAbout = volunteer.infoAbout;
+  const infoExperience = volunteer.infoExperience;
+  const lastName = volunteer.person.lastName;
+  const measlesVaccination = volunteer.statusVaccination;
+  const opportunitiesApplied = [] as unknown as Option[];
+  const opportunitiesMatched = [] as unknown as Option[];
+  const phone = volunteer.person.phone;
+  const timelineLogs = [] as unknown as TimedText[];
+  return {
+    address,
+    comments,
+    email,
+    firstName,
+    goodConductCertificate,
+    infoAbout,
+    infoExperience,
+    lastName,
+    measlesVaccination,
+    opportunitiesApplied,
+    opportunitiesMatched,
+    phone,
+    timelineLogs,
+    ...volunteerListSerializer(volunteer),
+  };
+}
+
+function getByDay(rrule: string): ByDay {
+  if (!rrule) {
+    throw new Error("RRule is required to get ByDay");
+  }
+  const byDayPos = rrule.indexOf("BYDAY") + 6;
+  if (byDayPos < 6) {
+    return null;
+  }
+  const byDay = rrule.slice(byDayPos, byDayPos + 2);
+  if (!(byDay in ByDay)) {
+    throw new Error("RRule BYDAY value is not recognized");
+  }
+  return ByDay[byDay];
+}
+
+function getHour(date: Date): Hour {
+  if (!date) {
+    throw new Error("Date is required to get hour");
+  }
+  const hour = `H${String(date.getHours()).padStart(2, "0")}`;
+  return Hour[hour];
+}
+
+function getAvailability(timeTimeslot: TimeTimeslot[]): Availability[] {
+  return timeTimeslot.map(({ timeslot }): Availability => {
+    if (timeslot?.rrule && timeslot?.start && timeslot?.end) {
+      return {
+        day: getByDay(timeslot.rrule),
+        daytime: [getHour(timeslot.start), getHour(timeslot.end)] as Daytime,
+      };
+    }
+    if (timeslot?.occasional) {
+      return {
+        day: Occasionally.OCCASIONALLY,
+        daytime: [timeslot.occasional],
+      };
+    }
+
+    throw new Error("Timeslot is missing required fields");
+  });
+}
+
+function getLanguages(profileLanguage: ProfileLanguage[]) {
+  return profileLanguage.map((pl) => ({
+    title: pl.language.translation || pl.language.title,
+    proficiency: pl.proficiency,
+  }));
+}
+
+function getTitles<T>(profileItems: T[], entityName: string) {
+  return profileItems.map(
+    (pa) => pa[entityName].translation || pa[entityName].title,
+  );
+}
