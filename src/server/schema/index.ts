1878e9e2f21cdf5d42966e06939cb334ec2a2911
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-23 11:47:43 +0000

    add: single volunteer schema

--- src/server/schema/index.ts
+++ src/server/schema/index.ts
@@ -6,5 +6,9 @@ import volunteerSchemaJson from "./volunteer-api.json";
 export const volunteerResponseSchema =
   volunteerSchemaJson.definitions.ApiVolunteerGetList;
 
+import volunteerIdSchemaJson from "./volunteer-api-id.json";
+export const volunteerIdResponseSchema =
+  volunteerIdSchemaJson.definitions.ApiVolunteerGet;
+
 import volunteerFormSchemaJson from "./volunteer-form.json";
 export const volunteerFormSchema = volunteerFormSchemaJson;
