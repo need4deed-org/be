8f6b12d5e4a359a9f4d07f8d64427fdbc6418552
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-28 21:30:48 +0000

    add: opportunities seed

---
+++ src/data/seeds/opportunity.seed.ts
@@ -0,0 +1,66 @@
+import { DataSource } from "typeorm";
+
+import { OpportunityType, TranslatedIntoType } from "need4deed-sdk";
+import { seedOpportunitiesFile } from "../../config/constants";
+import Opportunity from "../entity/opportunity/opportunity.entity";
+import { readJsonAsync } from "../utils";
+import { OpportunityJSON } from "./types";
+import {
+  createDeal,
+  getCount,
+  getEnumValue,
+  getOrCreateAgent,
+  getRepository,
+} from "./utils";
+
+export async function seedOpportunities(dataSource: DataSource): Promise<void> {
+  if (!dataSource) {
+    throw new Error("DataSource is not initialized.");
+  }
+
+  const opportunityRepository = getRepository(dataSource, Opportunity);
+
+  const count = await getCount(opportunityRepository);
+  if (count !== 0) {
+    dataSource.logger.log("log", "Skipping seeding opportunities.");
+    return;
+  }
+
+  const opportunities = (await readJsonAsync(
+    seedOpportunitiesFile,
+  )) as OpportunityJSON[];
+
+  for (const opportunity of opportunities) {
+    try {
+      const title = opportunity.title;
+
+      if (!title) {
+        throw new Error("title is missing.");
+      }
+
+      const deal = await createDeal(opportunity.deal, dataSource);
+
+      const agent = await getOrCreateAgent(opportunity.agent, dataSource);
+
+      const newOpportunity = new Opportunity({
+        title,
+        type:
+          getEnumValue(OpportunityType, opportunity.info) ||
+          OpportunityType.GENERAL,
+        translationType:
+          getEnumValue(TranslatedIntoType, opportunity.translationType) ||
+          TranslatedIntoType.NO_TRANSLATION,
+        info: opportunity.info || "",
+        infoConfidential: opportunity.infoConfidential || "",
+        agent,
+        deal,
+      });
+      await opportunityRepository.save(newOpportunity);
+    } catch (error) {
+      dataSource.logger.log(
+        "log",
+        `Creation of opportunity ${opportunity?.title} rolled back due to error: ${error.message}`,
+      );
+    }
+  }
+}
