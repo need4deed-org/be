7e0906b748ff832d7fffd1609d9d095619e2e9ab
arturasmckwcz <arturasmckwcz@gmail.com>
2025-09-28 21:28:22 +0000

    add: dummy users

--- src/data/seeds/user.seed.ts
+++ src/data/seeds/user.seed.ts
@@ -1,12 +1,24 @@
 import { DataSource } from "typeorm";
 
+import Address from "../entity/location/address.entity";
+import Postcode from "../entity/location/postcode.entity";
 import Person from "../entity/person.entity";
 import User from "../entity/user.entity";
 import { Role } from "../types";
 import { hashPassword } from "../utils";
-import { getCount } from "./utils";
+import { getCount, getRepository } from "./utils";
 
 export async function seedUser(dataSource: DataSource) {
+  const userRepository = getRepository(dataSource, User);
+  const postcodeRepository = getRepository(dataSource, Postcode);
+  const addressRepository = getRepository(dataSource, Address);
+
+  const count = await getCount(userRepository);
+  if (count !== 0) {
+    dataSource.logger.log("log", "Skipping seeding users.");
+    return;
+  }
+
   const personAdmin = new Person({
     firstName: "John",
     middleName: "Admin",
@@ -21,33 +33,43 @@ export async function seedUser(dataSource: DataSource) {
     person: personAdmin,
   });
 
+  const personCoordinator = new Person({
+    firstName: "Sarah",
+    middleName: "Coordinator",
+    lastName: "Doe",
+  });
+
+  const coordinatorUser = new User({
+    email: "sarah.doe@need4deed.org",
+    password: await hashPassword("no_password"),
+    role: Role.COORDINATOR,
+    isActive: true,
+    person: personCoordinator,
+  });
+
+  const postcode12345 = await postcodeRepository.findOne({
+    where: { value: "12345" },
+  });
+  const address = new Address({ postcode: postcode12345, title: "Dummy" });
+  await addressRepository.save(address);
   const personUser = new Person({
     firstName: "Anna",
     middleName: "User",
     lastName: "Doe",
+    email: "anna.doe@need4deed.org",
+    address,
   });
 
   const userUser = new User({
     email: "anna.doe@need4deed.org",
     password: await hashPassword("no_password"),
-    role: Role.COORDINATOR,
+    role: Role.USER,
     isActive: true,
     person: personUser,
   });
 
-  const userRepository = dataSource.getRepository(User);
-  if (!userRepository) {
-    throw new Error("Skill entity is not initialized.");
-  }
-
-  const count = await getCount(userRepository);
-  if (count !== 0) {
-    dataSource.logger.log("log", "Skipping seeding users.");
-    return;
-  }
-
   try {
-    userRepository.save([userAdmin, userUser]);
+    await userRepository.save([userAdmin, coordinatorUser, userUser]);
   } catch (error) {
     dataSource.logger.log(
       "log",
