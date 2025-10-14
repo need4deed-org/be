ccd2f9a49b4674382c89c55908bba83e65e7e627
arturasmckwcz <arturasmckwcz@gmail.com>
2025-10-14 11:34:32 +0000

    add: swagger and register all schemas beforehand (for swagger)

--- src/server/schema/index.ts
+++ src/server/schema/index.ts
@@ -1,21 +1,3 @@
 export * from "./person.schema";
 export * from "./responseErrors";
 export * from "./user.schema";
-
-import volunteerSchemaJson from "./volunteer-api.json";
-export const volunteerResponseSchema =
-  volunteerSchemaJson.definitions.ApiVolunteerGetList;
-
-import volunteerIdSchemaJson from "./volunteer-api-id.json";
-export const volunteerIdResponseSchema =
-  volunteerIdSchemaJson.definitions.ApiVolunteerGet;
-
-import volunteerIdPatchSchemaJson from "./volunteer-api-id-part.json";
-export const volunteerIdPatchBodySchema =
-  volunteerIdPatchSchemaJson.definitions.ApiVolunteerBodyPart;
-
-import volunteerFormSchemaJson from "./volunteer-form.json";
-export const volunteerFormSchema = volunteerFormSchemaJson;
-
-import optionListsSchemaJson from "./option-lists.json";
-export const optionListsSchema = optionListsSchemaJson.definitions.OptionLists;
