e77937a1888296bde45273cb36dc90787e328596
arturasmckwcz <arturasmckwcz@gmail.com>
2025-11-19 15:26:10 +0000

    update: run DDL first then DML

--- data-bootstrap/bootstrap.sh
+++ data-bootstrap/bootstrap.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "Importing database dump..."
-PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -f /app/dump.sql
+echo "Importing database schema..."
+PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -f /app/schema.sql
 
 if [ $? -eq 0 ]; then
     echo "Database import completed successfully"
@@ -12,6 +12,18 @@ else
     echo "Import failed"
     exit 1
 fi
+
+echo "Importing database dump..."
+PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -f /app/data.sql
+
+if [ $? -eq 0 ]; then
+    echo "Database import completed successfully"
+  
+else
+    echo "Import failed"
+    exit 1
+fi
+
 # Clean up
 unset PGPASSWORD
 
