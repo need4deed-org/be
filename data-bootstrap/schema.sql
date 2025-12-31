5fbff15e05740831343619914e624a6d9eb503a4
arturasmckwcz <arturasmckwcz@gmail.com>
2025-12-31 09:09:48 +0000

    add: value to volunteer_status_type_enum

--- data-bootstrap/schema.sql
+++ data-bootstrap/schema.sql
@@ -363,6 +363,7 @@ ALTER TYPE public.volunteer_status_match_enum OWNER TO postgres;
 CREATE TYPE public.volunteer_status_type_enum AS ENUM (
     'accompanying',
     'regular',
+    'regular-accompanying',
     'events'
 );
 
