1a565a48483fb1a9cf9db9b8261592d4085146a7
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-05 20:11:49 +0000

    refactor: ditch enum for const assertion

--- src/server/types/enums.ts
+++ src/server/types/enums.ts
@@ -1,15 +1,18 @@
- 
-export enum RoutePrefix {
-  AUTH = "/auth",
-  COMMENT = "/comment",
-  HEALTH_CHECK = "/health-check",
-  LOGIN = "/login",
-  ME = "/me",
-  OPTION = "/option",
-  PERSON = "/person",
-  REFRESH = "/refresh",
-  SWAGGER = "/swagger",
-  USER = "/user",
-  VERIFY_EMAIL = "/verify-email",
-  VOLUNTEER = "/volunteer",
-}
+export const RoutePrefix = {
+  AUTH: "/auth",
+  COMMENT: "/comment",
+  DOC: "/doc",
+  HEALTH_CHECK: "/health-check",
+  LOGIN: "/login",
+  ME: "/me",
+  OPTION: "/option",
+  PERSON: "/person",
+  REFRESH: "/refresh",
+  SWAGGER: "/swagger",
+  USER: "/user",
+  VERIFY_EMAIL: "/verify-email",
+  VOLUNTEER: "/volunteer",
+} as const;
+
+// eslint-disable-next-line no-redeclare
+export type RoutePrefix = (typeof RoutePrefix)[keyof typeof RoutePrefix];
