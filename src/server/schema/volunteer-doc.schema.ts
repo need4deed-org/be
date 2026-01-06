12916733ba7e6d533670a7be7065908e9b4132ea
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-06 11:45:03 +0000

    add: schema for pre-signed url object

--- src/server/schema/volunteer-doc.schema.ts
+++ src/server/schema/volunteer-doc.schema.ts
@@ -7,9 +7,33 @@ export const volunteerDocSchemaGet200 = {
   required: ["message", "data"],
 };
 
-export const volunteerDocSchemaPostBody = { $ref: "ApiDocumentPost#" };
-export const volunteerDocSchemaPost200 = { url: { type: "string" } };
-export const volunteerDocSchemaUploadMeta200 = {
+export const volunteerDocSchemaGetMeta200 = {
+  type: "object",
+  properties: {
+    url: { type: "string" },
+    fields: {
+      type: "object",
+      properties: {
+        bucket: { type: "string" },
+        key: { type: "string" },
+        Policy: { type: "string" },
+        "X-Amz-Algorithm": { type: "string" },
+        "X-Amz-Credential": { type: "string" },
+        "X-Amz-Date": { type: "string" },
+        "X-Amz-Signature": { type: "string" },
+        "x-amz-meta-volunteer-id": { type: "string" },
+        "x-amz-meta-document-type": { $ref: "DocumentType#" },
+        "x-amz-meta-original-name": { type: "string" },
+        "x-amz-meta-mime-type": { type: "string" },
+        "x-amz-meta-s3-key": { type: "string" },
+      },
+      // required: [],
+    },
+  },
+  required: ["url", "fields"],
+};
+
+export const volunteerDocSchemaUploadMeta = {
   type: "object",
   properties: {
     mimeType: { type: "string" },
