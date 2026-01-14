464a01a0b388b36df559452c35f7c6c13b16c1ee
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-14 19:43:55 +0000

    add: entity appreciation

---
+++ src/data/entity/volunteer/appreciation.entity.ts
@@ -0,0 +1,69 @@
+import { ContactType, VolunteerStateAppreciationType } from "need4deed-sdk";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import Opportunity from "../opportunity/opportunity.entity";
+import User from "../user.entity";
+import Volunteer from "./volunteer.entity";
+
+@Entity()
+export default class Appreciation {
+  constructor(appreciation?: Partial<Appreciation>) {
+    if (appreciation) {
+      Object.assign(this, appreciation);
+    }
+  }
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "enum", enum: ContactType })
+  title: VolunteerStateAppreciationType;
+
+  @Column({ type: "timestamp", nullable: true })
+  dateDue?: Date;
+
+  @Column({ type: "timestamp", nullable: true })
+  dateDelivery?: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => Opportunity, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "opportunity_id" })
+  opportunity: Opportunity;
+
+  @Column({ nullable: true })
+  opportunityId: number;
+
+  @ManyToOne(() => Volunteer, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "volunteer_id" })
+  volunteer: Volunteer;
+
+  @Column({ nullable: true })
+  volunteerId: number;
+
+  @ManyToOne(() => User, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column({ nullable: true })
+  userId: number;
+}
