05a47c418c8a7054061594a269132c78af905016
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-26 12:44:04 +0000

    refactor: move migrations to post init and use lock

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,15 +1,15 @@
 import { dataSource } from "./data-source";
 import { createVolunteerListMV } from "./view/volunteer-list-mv";
 
-const lockNumber = 0xcdf2a8471b3e8f5fn;
+const lockNumber = 0x639b4e2a1c8d79a9n; // random BIGINT (for PostgreSQL)
 
 async function initDatabase() {
-  await dataSource.query(`SELECT pg_advisory_lock(${lockNumber})`);
-  try {
-    await dataSource.initialize();
-    if (dataSource.isInitialized) {
-      dataSource.logger.log("info", "Data Source has been initialized!");
-
+  await dataSource.initialize();
+  if (dataSource.isInitialized) {
+    dataSource.logger.log("info", "Data Source has been initialized!");
+    await dataSource.query(`SELECT pg_advisory_lock(${lockNumber})`);
+    dataSource.logger.log("info", "Acquired the lock for migrations");
+    try {
       await dataSource.runMigrations();
       dataSource.logger.log("info", "Run migrations");
 
@@ -17,15 +17,16 @@ async function initDatabase() {
       dataSource.logger.log("info", "Created MVs");
 
       dataSource.logger.log("info", "Seeded master data");
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      throw Error(
+        `Error occurred while initializing DataSource: ${error.message}`,
+      );
+    } finally {
+      await dataSource.query(`SELECT pg_advisory_unlock(${lockNumber})`);
+      dataSource.logger.log("info", "Released the lock for migrations");
     }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    throw Error(
-      `Error occurred while initializing DataSource: ${error.message}`,
-    );
-  } finally {
-    await dataSource.query(`SELECT pg_advisory_unlock(${lockNumber})`);
   }
 }
 
