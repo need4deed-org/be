4cf9267ee56412ce1348a7c449863e536103f614
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-29 14:20:33 +0000

    update: schemas

--- src/server/schema/params.ts
+++ src/server/schema/params.ts
@@ -14,3 +14,12 @@ export const idTypeParamSchema = {
   },
   required: ["id", "type"],
 };
+
+export const idmM2mIdParamSchema = {
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    m2mId: { type: "integer" },
+  },
+  required: ["id", "m2mId"],
+};
