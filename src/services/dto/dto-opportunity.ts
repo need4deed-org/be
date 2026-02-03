09b558a78e2e5d13c9698bb89f673718db40b7de
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-03 16:23:03 +0000

    add: dto for opp list

---
+++ src/services/dto/dto-opportunity.ts
@@ -0,0 +1,14 @@
+import { ApiOpportunityGetList } from "need4deed-sdk";
+import Opportunity from "../../data/entity/opportunity/opportunity.entity";
+
+export function dtoOpportunityGetList(
+  opportunity: Opportunity,
+): ApiOpportunityGetList {
+  return {
+    id: opportunity.id,
+    title: opportunity.title,
+    category: { id: opportunity.deal.profile.categoryId },
+    volunteerType: opportunity.deal.profile.volunteeringType,
+    statusOpportunity: opportunity.status,
+  };
+}
