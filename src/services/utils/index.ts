9be815b1e8ff520b6038aa534feb22c18abd77bd
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-08 23:11:06 +0000

    add: DeepMerge util

--- src/services/utils/index.ts
+++ src/services/utils/index.ts
@@ -74,3 +74,25 @@ export function getErrorStatusCode(error: BaseError | Error): number {
   }
   return 500; // Default to Internal Server Error
 }
+
+export function isObject(item) {
+  return item && typeof item === "object" && !Array.isArray(item);
+}
+
+export function deepMerge(target, source) {
+  const output = Object.assign({}, target);
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach((key) => {
+      if (isObject(source[key])) {
+        if (!(key in target)) {
+          Object.assign(output, { [key]: source[key] });
+        } else {
+          output[key] = deepMerge(target[key], source[key]);
+        }
+      } else {
+        Object.assign(output, { [key]: source[key] });
+      }
+    });
+  }
+  return output;
+}
