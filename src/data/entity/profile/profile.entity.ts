576426127db100f9532fb7f80a1c3c5ac299d438
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-09 20:33:19 +0000

    add: and register accompanying and relation

--- src/data/entity/profile/profile.entity.ts
+++ src/data/entity/profile/profile.entity.ts
@@ -12,6 +12,7 @@ import Deal from "../deal.entity";
 import ProfileActivity from "../m2m/profile-activity";
 import ProfileLanguage from "../m2m/profile-language";
 import ProfileSkill from "../m2m/profile-skill";
+import Accompanying from "../opportunity/accompanying.entity";
 import Category from "./category.entity";
 
 @Entity()
@@ -34,6 +35,15 @@ export default class Profile {
   @IsOptional()
   @IsEnum(ProfileVolunteeringType)
   volunteeringType?: ProfileVolunteeringType;
+
+  @ManyToOne(() => Accompanying, (accompanying) => accompanying.profile, {
+    nullable: true,
+  })
+  @JoinColumn({ name: "accompanying_id" })
+  accompanying?: Accompanying;
+
+  @Column({ nullable: true })
+  accompanyingId: number;
 
   @ManyToOne(() => Category, (category) => category.profile, {
     nullable: true,
