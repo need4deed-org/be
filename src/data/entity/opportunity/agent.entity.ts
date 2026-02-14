1f8d809a3a1b8a46afcccc05187b9ffd996e398a
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-14 22:51:55 +0000

    update: agent entity

--- src/data/entity/opportunity/agent.entity.ts
+++ src/data/entity/opportunity/agent.entity.ts
@@ -1,13 +1,21 @@
-import { IsEnum } from "class-validator";
+import { IsArray, IsEnum, IsOptional, IsString } from "class-validator";
+import {
+  AgentOperatorType,
+  AgentServiceType,
+  AgentTrustType,
+  AgentType,
+  AgentVolunteerSearchType,
+} from "need4deed-sdk";
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
-import { AgentOperatorType, AgentType } from "../../types";
 import Postcode from "../location/postcode.entity";
 import AgentPostcode from "../m2m/agent-postcode";
 import Person from "../person.entity";
@@ -27,6 +35,11 @@ export default class Agent {
   @Column({ unique: true })
   title: string;
 
+  @Column({ nullable: true })
+  @IsOptional()
+  @IsString()
+  website?: string;
+
   @Column({
     type: "enum",
     enum: AgentType,
@@ -36,11 +49,49 @@ export default class Agent {
   @IsEnum(AgentType)
   type: AgentType;
 
-  @Column({ type: "enum", enum: AgentOperatorType, nullable: false })
+  @Column({
+    type: "enum",
+    enum: AgentTrustType,
+    default: AgentTrustType.UNKNOWN,
+  })
+  trustLevel: AgentTrustType;
+
+  @Column({
+    type: "enum",
+    enum: AgentVolunteerSearchType,
+    default: AgentVolunteerSearchType.NOT_NEEDED,
+  })
+  searchStatus: AgentVolunteerSearchType;
+
+  @Column({
+    type: "text",
+    array: true,
+    transformer: {
+      to: (value: AgentServiceType[]) => value,
+      from: (value: unknown) => value as AgentServiceType[],
+    },
+    nullable: true,
+  })
+  @IsOptional()
+  @IsArray()
+  @IsEnum(AgentServiceType, { each: true })
+  services: AgentServiceType[];
+
+  @Column({
+    type: "enum",
+    enum: AgentOperatorType,
+    default: AgentOperatorType.ORGANIZATION,
+  })
   operatorType: AgentOperatorType;
 
   @Column()
   operatorId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @ManyToOne(() => Person)
   @JoinColumn({ name: "person_id" })
---
+++ src/data/migrations/1771086827549-update-agent-entity.ts
@@ -0,0 +1,83 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateAgentEntity1771086827549 implements MigrationInterface {
+  name = "UpdateAgentEntity1771086827549";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "agent" ADD "website" character varying`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."agent_trust_level_enum" AS ENUM('high', 'low', 'unknown')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ADD "trust_level" "public"."agent_trust_level_enum" NOT NULL DEFAULT 'unknown'`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."agent_search_status_enum" AS ENUM('searching', 'not-needed', 'volunteers-found')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ADD "search_status" "public"."agent_search_status_enum" NOT NULL DEFAULT 'not-needed'`,
+    );
+    await queryRunner.query(`ALTER TABLE "agent" ADD "services" text array`);
+    await queryRunner.query(
+      `ALTER TABLE "agent" ADD "created_at" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ADD "updated_at" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "organization" ADD "website" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TYPE "public"."agent_type_enum" RENAME TO "agent_type_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."agent_type_enum" AS ENUM('rac', 'ngo')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" TYPE "public"."agent_type_enum" USING "type"::"text"::"public"."agent_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" SET DEFAULT 'rac'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."agent_type_enum_old"`);
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "operator_type" SET DEFAULT 'organization'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "operator_type" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."agent_type_enum_old" AS ENUM('NGO', 'RAC')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" TYPE "public"."agent_type_enum_old" USING "type"::"text"::"public"."agent_type_enum_old"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "agent" ALTER COLUMN "type" SET DEFAULT 'RAC'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."agent_type_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."agent_type_enum_old" RENAME TO "agent_type_enum"`,
+    );
+    await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "website"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "updated_at"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "created_at"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "services"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "search_status"`);
+    await queryRunner.query(`DROP TYPE "public"."agent_search_status_enum"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "trust_level"`);
+    await queryRunner.query(`DROP TYPE "public"."agent_trust_level_enum"`);
+    await queryRunner.query(`ALTER TABLE "agent" DROP COLUMN "website"`);
+  }
+}
