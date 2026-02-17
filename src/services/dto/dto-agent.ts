fab7868ab0a7be01f6890da529390f2beb98c36a
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-17 09:55:26 +0000

    add: dto fields

--- src/services/dto/dto-agent.ts
+++ src/services/dto/dto-agent.ts
@@ -1,10 +1,25 @@
 import { ApiAgentGetList } from "need4deed-sdk";
 import Agent from "../../data/entity/opportunity/agent.entity";
 
-export function dtoAgentGetList(agent: Agent): ApiAgentGetList {
+export function dtoAgentGetList(
+  agent: Agent & { activeVolunteers: number },
+): ApiAgentGetList {
   return {
     id: agent.id,
     title: agent.title,
     type: agent.type,
+    activeVolunteers: agent.activeVolunteers,
+    address: {
+      id: agent.addressId,
+      street: agent.address?.street,
+      city: agent.address?.city,
+      postcode: {
+        id: agent.address?.postcodeId,
+        code: agent.address?.postcode?.value,
+        latitude: undefined,
+        longitude: undefined,
+      },
+    },
+    district: { id: agent.districtId, title: { de: agent.district?.title } },
   };
 }
