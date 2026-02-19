860f6e6292d85b7dcfb56c5d7fd81deff7579b4f
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-19 21:16:29 +0000

    remove: redundant code

--- src/services/dto/dto-opportunity.ts
+++ src/services/dto/dto-opportunity.ts
@@ -2,11 +2,7 @@ import {
   ApiOpportunityGetList,
   ApiVolunteerOpportunityGetList,
 } from "need4deed-sdk";
-import { dataSource } from "../../data/data-source";
 import Opportunity from "../../data/entity/opportunity/opportunity.entity";
-import Profile from "../../data/entity/profile/profile.entity";
-import { categorize } from "../../data/lib";
-import { getRepository } from "../../data/utils";
 import { tryCatchFn } from "../utils";
 import { getAvailability } from "./utils";
 
@@ -55,30 +51,4 @@ export function dtoVolunteerOpportunityGetList(
       })),
     availability: getAvailabilityTryCatch(opportunity.deal.time?.timeTimeslot),
   };
-}
-
-export function dtoOpportunitiesCalcCategory<T>(
-  opportunities: Opportunity[],
-  dtoFn: (opportunity: Opportunity) => T,
-): T[] {
-  const updates: Profile[] = [];
-  const opportunitiesUpdated = opportunities.map((opportunity) => {
-    if (opportunity.deal.profile.categoryId) {
-      return dtoFn(opportunity);
-    }
-
-    opportunity.deal.profile.categoryId = categorize(
-      opportunity.deal.profile.profileActivity.map(
-        ({ activity }) => activity.categoryId,
-      ),
-    );
-    updates.push(opportunity.deal.profile);
-    return dtoFn(opportunity);
-  });
-
-  if (updates.length > 0) {
-    const profileRepository = getRepository(dataSource, Profile);
-    profileRepository.save(updates);
-  }
-  return opportunitiesUpdated;
 }
