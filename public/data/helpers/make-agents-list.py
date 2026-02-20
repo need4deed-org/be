f6b3f17852c5defbae3b8bd810328a32796c055a
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-20 11:09:53 +0000

    add: scrambling of pii

--- public/data/helpers/make-agents-list.py
+++ public/data/helpers/make-agents-list.py
@@ -3,7 +3,7 @@ import sys
 import uuid
 
 from utils import (get_address, get_email, get_list, get_name_fields,
-                   get_string_or_null)
+                   get_parsed_cli, get_string_or_null)
 
 
 class Agent:
@@ -11,8 +11,9 @@ class Agent:
     A class to handle RACs data processing.
     """
 
-    def __init__(self, agent):
+    def __init__(self, agent, scramble):
         self.agent = agent
+        self.scramble = scramble
 
     def get_person_data(self):
         """
@@ -23,9 +24,9 @@ class Agent:
         
         return {
             "id": uuid.uuid4().hex,
-            **get_name_fields(self.agent.get("Volunteer Coordinator", "")),
-            "email": get_email(self.agent.get("Email (volunteer coordinator)", "")),
-            "phone": get_string_or_null(self.agent.get("Moblie", "")),
+            **get_name_fields(self.agent.get("Volunteer Coordinator", ""), scramble=self.scramble),
+            "email": get_email(self.agent.get("Email (volunteer coordinator)", ""), scramble=self.scramble),
+            "phone": get_string_or_null(self.agent.get("Moblie", ""), scramble=self.scramble),
         }
     
     def get_organization_data(self):
@@ -38,9 +39,9 @@ class Agent:
         return {
             "id": uuid.uuid4().hex,
             "title": get_string_or_null(self.agent.get("Operator", "")),
-            "address": {"id": uuid.uuid4().hex, **get_address(self.agent.get("Address", ""))},
-            "phone": get_string_or_null(self.agent.get("Phone", "")),
-            "email": get_email(self.agent.get("Email", "")),
+            "address": {"id": uuid.uuid4().hex, **get_address(self.agent.get("Address", ""), scramble=self.scramble)}, # type: ignore
+            "phone": get_string_or_null(self.agent.get("Phone", ""), scramble=self.scramble),
+            "email": get_email(self.agent.get("Email", ""), scramble=self.scramble),
         }
     def get_title(self):
         return get_string_or_null(self.agent.get("Name", ""))
@@ -51,14 +52,14 @@ class Agent:
     def get_district(self):
         return get_string_or_null(self.agent.get("District", ""))
 
-def get_agent(agent):
+def get_agent(agent, scramble=False):
     """
     Processes a agent object and returns a structured JSON.
     """
     if not agent or not isinstance(agent, dict):
         return None
 
-    agent_instance = Agent(agent)
+    agent_instance = Agent(agent, scramble)
     return {
         "id": uuid.uuid4().hex,
         "title": agent_instance.get_title(),
@@ -69,14 +70,16 @@ def get_agent(agent):
     }
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    scramble, file_url = get_parsed_cli(sys.argv) 
+
+    if not file_url:
         print(f"Usage: python {sys.argv[0]} <path_to_agent_json>")
         sys.exit(1)
 
     try:
-        with open(sys.argv[1], 'r') as file:
+        with open(file_url, 'r') as file:
             agent_data = json.load(file)
-            result = [get_agent(agent) for agent in agent_data]
+            result = [get_agent(agent, scramble=bool(scramble)) for agent in agent_data]
             print(json.dumps(result, indent=4))
     except Exception as e:
         print(f"Error processing volunteer data: {e}")
