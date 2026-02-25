ffe48d0ffb5709cde0d56459b528ef9029f8675c
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-25 20:54:26 +0000

    refactor: formatted

--- public/data/helpers/make-opportunities-list.py
+++ public/data/helpers/make-opportunities-list.py
@@ -1,9 +1,17 @@
 import json
 import sys
 
-from utils import (get_email, get_language_split, get_list, get_list_item_safe,
-                   get_name_fields, get_parsed_cli, get_string_or_null,
-                   get_timeslot_data, is_valid_js_date)
+from utils import (
+    get_email,
+    get_language_split,
+    get_list,
+    get_list_item_safe,
+    get_name_fields,
+    get_parsed_cli,
+    get_string_or_null,
+    get_timeslot_data,
+    is_valid_js_date,
+)
 
 
 class Opportunity:
@@ -21,28 +29,31 @@ class Opportunity:
         """
         if not self.opportunity or not isinstance(self.opportunity, dict):
             return None
-        
+
         def get_agent_title():
             """
             Calculates agent title from the opportunity dictionary.
             """
             title = self.opportunity.get("Accommodation Centre", "")
             if title:
-                return get_string_or_null(title[:title.find(" (")], scramble=self.scramble)
+                return get_string_or_null(
+                    title[: title.find(" (")], scramble=self.scramble
+                )
 
             return get_string_or_null(
                 f"noname {get_list_item_safe(self.opportunity.get('RAC Contact','').split('<|>'), 1) or ''}".strip(),
                 scramble=self.scramble,
-                )
-
+            )
 
         def get_person_data():
             """
             Extracts and formats person data from a volunteer dictionary.
             """
-            rac_contact = self.opportunity.get("RAC Contact","").split("<|>")
+            rac_contact = self.opportunity.get("RAC Contact", "").split("<|>")
             if len(rac_contact) == 5:
-                name, email, phone, _, postcode = self.opportunity.get("RAC Contact","").split("<|>")
+                name, email, phone, _, postcode = self.opportunity.get(
+                    "RAC Contact", ""
+                ).split("<|>")
                 return {
                     **get_name_fields(name, scramble=self.scramble),
                     "email": get_email(email, scramble=self.scramble),
@@ -56,11 +67,10 @@ class Opportunity:
 
         title = get_agent_title()
         person = get_person_data()
-        organization = {
-            "title": get_agent_title(),
-            "person": person
-        }
-        postcode = person["address"]["postcode"] if person and person.get("address") else None
+        organization = {"title": get_agent_title(), "person": person}
+        postcode = (
+            person["address"]["postcode"] if person and person.get("address") else None
+        )
 
         return {
             "title": title,
@@ -75,14 +85,23 @@ class Opportunity:
         """
         if not self.opportunity or not isinstance(self.opportunity, dict):
             return None
-    
+
         def get_profile_data():
             """
             Extracts profile data from the opportunity dictionary.
             """
-            activities = [activity.strip() for activity in self.opportunity.get("Type", "").split(",")]
-            skills = [activity.strip() for activity in self.opportunity.get("Skills", "").split(",")]
-            languages = [get_list(get_language_split(language.strip())) for language in self.opportunity.get("Refugee Languages", "").split(",")]
+            activities = [
+                activity.strip()
+                for activity in self.opportunity.get("Type", "").split(",")
+            ]
+            skills = [
+                activity.strip()
+                for activity in self.opportunity.get("Skills", "").split(",")
+            ]
+            languages = [
+                get_list(get_language_split(language.strip()))
+                for language in self.opportunity.get("Refugee Languages", "").split(",")
+            ]
 
             return {
                 "info": get_string_or_null(self.opportunity.get("Comments", "")),
@@ -90,7 +109,7 @@ class Opportunity:
                 "skills": get_list(skills),
                 "languages": get_list(languages),
             }
-        
+
         def get_time_data():
             """
             Extracts time data from the opportunity dictionary.
@@ -109,15 +128,18 @@ class Opportunity:
             return {
                 "timeslots": [timeslot],
             }
-        
+
         def get_location_data():
             """
             Extracts location data from the opportunity dictionary.
             """
-            districts = [district.strip() for district in self.opportunity.get("District VO", "").split(",")]
+            districts = [
+                district.strip()
+                for district in self.opportunity.get("District VO", "").split(",")
+            ]
 
             return {"districts": get_list(districts)}
-        
+
         return {
             "type": "opportunity",
             "postcode": get_string_or_null(self.get_postcode()),
@@ -136,19 +158,28 @@ class Opportunity:
         """
         Extracts info from the opportunity dictionary.
         """
-        return get_string_or_null(self.opportunity.get("VO information", ""), scramble=self.scramble)
+        return get_string_or_null(
+            self.opportunity.get("VO information", ""), scramble=self.scramble
+        )
 
     def get_info_confidential(self):
         """
         Extracts confidential info from the opportunity dictionary.
         """
-        return get_string_or_null(self.opportunity.get("AA information", ""), scramble=self.scramble)
+        return get_string_or_null(
+            self.opportunity.get("AA information", ""), scramble=self.scramble
+        )
 
     def get_status(self):
         """
         Extracts Status from the opportunity dictionary.
         """
         return get_string_or_null(self.opportunity.get("Status", ""))
+
+    def get_number_volunteers(self):
+        return get_string_or_null(
+            self.opportunity.get("Number of volunteers needed", "")
+        )
 
     def get_type(self):
         """
@@ -161,7 +192,7 @@ class Opportunity:
         Extracts translation type from the opportunity dictionary.
         """
         return get_string_or_null(self.opportunity.get("Translation", ""))
-    
+
     def get_postcode(self):
         """
         Extracts postcode from the opportunity dictionary.
@@ -178,6 +209,7 @@ class Opportunity:
 
         return None
 
+
 def get_opportunity(opportunity, scramble=False):
     """
     Processes an opportunity object and returns a structured JSON.
@@ -191,6 +223,7 @@ def get_opportunity(opportunity, scramble=False):
         "status": opportunity_instance.get_status(),
         "type": opportunity_instance.get_type(),
         "translationType": opportunity_instance.get_translation_type(),
+        "numberVolunteers": opportunity_instance.get_number_volunteers(),
         "info": opportunity_instance.get_info(),
         "infoConfidential": opportunity_instance.get_info_confidential(),
         "deal": opportunity_instance.get_deal_data(),
@@ -199,16 +232,19 @@ def get_opportunity(opportunity, scramble=False):
 
 
 if __name__ == "__main__":
-    scramble, file_url = get_parsed_cli(sys.argv) 
+    scramble, file_url = get_parsed_cli(sys.argv)
 
     if not file_url:
         print(f"Usage: python {sys.argv[0]} <path_to_volunteer_json>")
         sys.exit(1)
 
     try:
-        with open(file_url, 'r') as file:
+        with open(file_url, "r") as file:
             opportunity_data = json.load(file)
-            result = [get_opportunity(opportunity, bool(scramble)) for opportunity in opportunity_data]
+            result = [
+                get_opportunity(opportunity, bool(scramble))
+                for opportunity in opportunity_data
+            ]
             print(json.dumps(result, indent=4))
     except Exception as e:
         print(f"Error processing opportunity data: {e}")
