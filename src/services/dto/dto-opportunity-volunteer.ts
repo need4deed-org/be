a46a9c1282a0d66978c050a872072437828181c7
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-04 15:15:27 +0000

    add: DTO

--- src/services/dto/dto-opportunity-volunteer.ts
+++ src/services/dto/dto-opportunity-volunteer.ts
@@ -1,7 +1,10 @@
-import { ApiOpportunityVolunteerGet } from "need4deed-sdk";
+import {
+  ApiOpportunityVolunteerGet,
+  ApiVolunteerOpportunityGet,
+} from "need4deed-sdk";
 import OpportunityVolunteer from "../../data/entity/m2m/opportunity-volunteer";
 
-export function opportunityVolunteerDTO(
+export function volunteerOpportunityVolunteerDTO(
   opportunityVolunteer: OpportunityVolunteer,
 ): ApiOpportunityVolunteerGet {
   if (!(opportunityVolunteer instanceof OpportunityVolunteer)) {
@@ -10,10 +13,29 @@ export function opportunityVolunteerDTO(
 
   return {
     id: opportunityVolunteer.id,
+    status: opportunityVolunteer.status,
     volunteerId: opportunityVolunteer.volunteerId,
     opportunityId: opportunityVolunteer.opportunityId,
-    title: opportunityVolunteer.opportunity.title,
-    status: opportunityVolunteer.status,
     updatedAt: opportunityVolunteer.updatedAt,
+    title: opportunityVolunteer.opportunity.title,
+  };
+}
+
+export function opportunityOpportunityVolunteerDTO(
+  opportunityVolunteer: OpportunityVolunteer,
+): ApiVolunteerOpportunityGet {
+  if (!(opportunityVolunteer instanceof OpportunityVolunteer)) {
+    throw new Error("Wrong opportunity-volunteer format.");
+  }
+
+  return {
+    id: opportunityVolunteer.id,
+    status: opportunityVolunteer.status,
+    volunteerId: opportunityVolunteer.volunteerId,
+    opportunityId: opportunityVolunteer.opportunityId,
+    updatedAt: opportunityVolunteer.updatedAt,
+    name: opportunityVolunteer.volunteer.person.name,
+    volunteeringType: opportunityVolunteer.volunteer.statusType,
+    engagement: opportunityVolunteer.volunteer.statusEngagement,
   };
 }
