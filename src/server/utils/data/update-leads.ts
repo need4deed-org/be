ef0b2a74da5b0661abca2b0a96476b27741b0bdd
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-04 07:52:49 +0000

    add: data helpers and refactor

---
+++ src/server/utils/data/update-leads.ts
@@ -0,0 +1,10 @@
+import { dataSource } from "../../../data/data-source";
+import LeadFrom from "../../../data/entity/lead.entity";
+
+export async function updateLeads(leads: LeadFrom[]): Promise<void> {
+  const leadFromRepository = dataSource.getRepository(LeadFrom);
+  for (const lead of leads) {
+    lead.count += 1;
+  }
+  await leadFromRepository.save(leads);
+}
