56c6d1404ca7c185e6f573a9099dc4b14a1075b8
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-04 18:19:02 +0000

    refactor: move away m2m id endpoints

--- src/server/routes/opportunity/opportunity-volunteer.routes.ts
+++ src/server/routes/opportunity/opportunity-volunteer.routes.ts
@@ -1,32 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import {
-  ApiOpportunityVolunteerGet,
-  ApiVolunteerOpportunityGet,
-  OpportunityVolunteerStatusType,
-} from "need4deed-sdk";
-import { BadRequestError, NotFoundError } from "../../../config/error/fastify";
-import {
-  opportunityOpportunityVolunteerDTO,
-  volunteerOpportunityVolunteerDTO,
-} from "../../../services";
-import {
-  idmM2mIdParamSchema,
-  idParamSchema,
-  responseErrors,
-  responseSchema,
-} from "../../schema";
+import { ApiVolunteerOpportunityGet } from "need4deed-sdk";
+import { BadRequestError } from "../../../config/error/fastify";
+import { opportunityOpportunityVolunteerDTO } from "../../../services";
+import { idParamSchema, responseSchema } from "../../schema";
 
 const msg400 = "URL param must ba a positive number";
-
-const msg404 = (m2mId: number, volunteerId: number) =>
-  `There's no M2M relation id:${m2mId} for volunteer id:${volunteerId} to an opportunity`;
-
-const msg200 = (
-  opportunityId: number,
-  volunteerId: number,
-  action: "updated" | "deleted",
-) =>
-  `Relation for volunteer id:${volunteerId} and opportunity id:${opportunityId} has been ${action}.`;
 
 export default function opportunityOpportunityVolunteerRoutes(
   fastify: FastifyInstance,
@@ -56,7 +34,7 @@ export default function opportunityOpportunityVolunteerRoutes(
         where: {
           opportunityId,
         },
-        relations: ["volunteer"],
+        relations: ["volunteer.person"],
       });
 
       const data = volunteers.map(opportunityOpportunityVolunteerDTO);
@@ -64,93 +42,6 @@ export default function opportunityOpportunityVolunteerRoutes(
       return reply.status(200).send({
         message: `Volunteers for opportunity id:${opportunityId}.`,
         data,
-      });
-    },
-  );
-
-  fastify.patch<{
-    Params: { id: number; m2mId: number };
-    Reply: { message: string; data: ApiOpportunityVolunteerGet };
-    Body: { status: OpportunityVolunteerStatusType };
-  }>(
-    "/:m2mId",
-    {
-      schema: {
-        params: idmM2mIdParamSchema,
-        body: { $ref: "ApiVolunteerOpportunityPatch#" },
-        response: responseSchema("ApiVolunteerOpportunityGet#"),
-      },
-    },
-    async (request, reply) => {
-      const { id: volunteerId, m2mId } = request.params;
-      if (volunteerId <= 0 || m2mId <= 0) {
-        throw new BadRequestError(msg400);
-      }
-
-      const opportunityVolunteerRepository =
-        fastify.db.opportunityVolunteerRepository;
-
-      const opportunity = await opportunityVolunteerRepository.findOne({
-        where: { id: m2mId },
-        relations: ["opportunity"],
-      });
-
-      if (!opportunity) {
-        throw new NotFoundError(msg404(m2mId, volunteerId));
-      }
-
-      opportunityVolunteerRepository.merge(opportunity, request.body);
-      await opportunityVolunteerRepository.save(opportunity, { reload: true });
-
-      const data = volunteerOpportunityVolunteerDTO(opportunity);
-      return reply.status(200).send({
-        message: msg200(opportunity.opportunityId, volunteerId, "updated"),
-        data,
-      });
-    },
-  );
-
-  fastify.delete<{
-    Params: { id: number; m2mId: number };
-    Reply: { message: string };
-  }>(
-    "/:m2mId",
-    {
-      schema: {
-        params: idmM2mIdParamSchema,
-        response: {
-          200: {
-            type: "object",
-            properties: {
-              message: { type: "string" },
-            },
-            required: ["message"],
-          },
-          ...responseErrors,
-        },
-      },
-    },
-    async (request, reply) => {
-      const { id: volunteerId, m2mId } = request.params;
-      if (volunteerId <= 0 || m2mId <= 0) {
-        throw new BadRequestError("Param id must ba a positive number");
-      }
-
-      const opportunityVolunteerRepository =
-        fastify.db.opportunityVolunteerRepository;
-
-      const opportunity = await opportunityVolunteerRepository.findOne({
-        where: { id: m2mId },
-      });
-
-      if (!opportunity) {
-        throw new NotFoundError(msg404(m2mId, volunteerId));
-      }
-
-      await opportunityVolunteerRepository.delete({ id: m2mId });
-
-      return reply.status(200).send({
-        message: msg200(opportunity.opportunityId, volunteerId, "deleted"),
       });
     },
   );
