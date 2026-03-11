3f78f76f07086d5cced5bf6c93d8f018c8d26093
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-11 08:16:00 +0000

    add: body parsing util

---
+++ src/server/utils/data/get-opportunity-patch-data.ts
@@ -0,0 +1,63 @@
+import { ApiOpportunityPatch, LangPurpose } from "need4deed-sdk";
+import { BadRequestError } from "../../../config";
+import Accompanying from "../../../data/entity/opportunity/accompanying.entity";
+import Agent from "../../../data/entity/opportunity/agent.entity";
+import { getNameFields } from "../../../services";
+import { getDateObj } from "../../../services/utils";
+import { getEmptyPropsNull } from "../common";
+
+export function parseOpportunity(body: ApiOpportunityPatch) {
+  if (!body) {
+    throw new BadRequestError("invalid body for parseOpportunity.");
+  }
+  return {
+    ...getEmptyPropsNull({
+      opportunity: {
+        status: body.statusOpportunity,
+        numberVolunteers: body.numberVolunteers,
+        info: body.description,
+        infoConfidential: body.description,
+      },
+      contact: body?.contact
+        ? {
+            id: body?.contact?.id,
+            ...getNameFields(body.contact.name),
+            phone: body?.contact?.phone,
+            email: body?.contact?.email,
+            preferredCommunicationType: body?.contact?.waysToContact,
+          }
+        : {},
+      agent: body?.agent
+        ? ({
+            title: body?.agent?.name,
+          } as Partial<Agent>)
+        : {},
+      accompanying: body?.accompanyingDetails
+        ? ({
+            address: body?.accompanyingDetails?.appointmentAddress,
+            date: getDateObj(
+              body?.accompanyingDetails?.appointmentDate,
+              body?.accompanyingDetails?.appointmentTime,
+            ),
+            phone: body?.accompanyingDetails?.refugeeNumber,
+            name: body?.accompanyingDetails?.refugeeName,
+            languageToTranslate:
+              body?.accompanyingDetails?.languagesToTranslate?.join(", "),
+          } as Partial<Accompanying>)
+        : {},
+      languages: [
+        ...(body?.languagesMain || []).map((l) => ({
+          id: l.id,
+          purpose: LangPurpose.GENERAL,
+        })),
+        ...(body?.languagesResidents || []).map((l) => ({
+          id: l.id,
+          purpose: LangPurpose.TRANSLATION,
+        })),
+      ] as Array<{ id: number }>,
+      skills: (body?.skills || []) as Array<{ id: number }>,
+      activities: (body?.activities || []) as Array<{ id: number }>,
+      schedule: (body.schedule || []) as Array<{ id: number }>,
+    }),
+  };
+}
