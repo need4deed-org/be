0948dc0687a060e263cb4c4271ea9a53ca55fda9
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-23 14:04:56 +0000

    refactor: add schemas

--- src/server/routes/health.ts
+++ src/server/routes/health.ts
@@ -1,41 +1,18 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import fp from "fastify-plugin";
-import { responseErrors } from "../schema/responseErrors";
-import { RoutePrefix } from "../types";
+import { responseSchema } from "../schema";
+import { ReplyMessage } from "../types";
 
-async function healthRoutes(
+export default async function healthRoutes(
   fastify: FastifyInstance,
-  options: FastifyPluginOptions,
+  _options: FastifyPluginOptions,
 ) {
-  const prefixedPath = options.prefix || RoutePrefix.HEALTH_CHECK;
-
-  const schema = {
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          message: { type: "string" },
-        },
-        required: ["message"],
-      },
-      ...responseErrors,
-    },
-  };
-
-  fastify.get(prefixedPath, { schema }, async (request, reply) => {
-    try {
+  fastify.get<{ Reply: ReplyMessage }>(
+    "/",
+    { schema: { response: responseSchema("") } },
+    async (_request, reply) => {
       return reply.status(200).send({
         message: "Need4Deed API v1 is up and running.",
       });
-    } catch (error) {
-      fastify.log.error(`Health check error: ${error}`);
-      return reply.status(500).send({
-        message: "Internal server error.",
-      });
-    }
-  });
+    },
+  );
 }
-
-export default fp(healthRoutes, {
-  name: "health-routes",
-});
