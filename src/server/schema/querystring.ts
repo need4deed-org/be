2242e6e74dd46def124a36347cbcb14675c7dbf9
arturasmckwcz <arturasmckwcz@gmail.com>
2026-04-07 12:53:58 +0000

    update: schema to take filters

--- src/server/schema/querystring.ts
+++ src/server/schema/querystring.ts
@@ -1,37 +1,49 @@
+import { SortOrder } from "need4deed-sdk";
 import { getRef } from "../utils";
 
 const paginationProps = {
   page: { type: "integer", minimum: 1 },
   limit: { type: "integer", minimum: 1, maximum: 120 },
 };
-const languageProp = { language: { type: "string" } };
+const langProp = { language: { type: "string" } };
+
+const sortOrderProps = {
+  sortOrder: {
+    type: "string",
+    enum: Object.values(SortOrder),
+  },
+};
 
 export const opportunityListQuerySchema = {
   type: "object",
   properties: {
     ...paginationProps,
-    ...languageProp,
-    type: {
-      anyOf: [
-        getRef("OpportunityType#"),
-        { type: "array", items: getRef("OpportunityType#") },
-      ],
+    ...sortOrderProps,
+    ...langProp,
+    filter: {
+      type: "object",
+      properties: {
+        language: { type: "array", items: { type: "string" } },
+        district: { type: "array", items: { type: "string" } },
+        type: {
+          type: "array",
+          items: getRef("OpportunityType#"),
+        },
+        status: {
+          type: "array",
+          items: getRef("OpportunityStatusType#"),
+        },
+      },
     },
-    status: {
-      anyOf: [
-        getRef("OpportunityStatusType#"),
-        { type: "array", items: getRef("OpportunityStatusType#") },
-      ],
-    },
+    additionalProperties: false,
   },
-  additionalProperties: false,
 };
 
 export const agentListQuerySchema = {
   type: "object",
   properties: {
     ...paginationProps,
-    ...languageProp,
+    ...langProp,
   },
   additionalProperties: false,
 };
@@ -40,7 +52,7 @@ export const volunteerOpportunityListQuerySchema = {
   type: "object",
   properties: {
     ...paginationProps,
-    ...languageProp,
+    ...langProp,
     type: getRef("VolunteerStateTypeType#"),
     status: getRef("OpportunityStatusType#"),
     agentId: { type: "number" },
