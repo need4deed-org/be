00eefc42f52f3111f163b57ff4d19a1070abad8d
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2026-05-06 17:40:41 +0200

    add: fn for district calc

---
+++ src/server/utils/data/add-district-to-opp.ts
@@ -0,0 +1,30 @@
+import Opportunity from "../../../data/entity/opportunity/opportunity.entity";
+import { getDistrictFromPostcode } from "../../../data/utils/get-district";
+
+export function getDistrictToOpportunityHandler() {
+  const updates: Opportunity[] = [];
+
+  return {
+    async addDistrictToOpportunity(
+      opportunity: Opportunity,
+    ): Promise<Opportunity> {
+      if (!opportunity || opportunity.districtId) {
+        return opportunity;
+      }
+      if (opportunity.agent?.districtId) {
+        opportunity.districtId = opportunity.agent.districtId;
+        updates.push(opportunity);
+        return opportunity;
+      }
+      const district = await getDistrictFromPostcode(
+        opportunity.agent?.address?.postcode!,
+      );
+      if (district) {
+        opportunity.district = district;
+        updates.push(opportunity);
+      }
+      return opportunity;
+    },
+    updates,
+  };
+}
